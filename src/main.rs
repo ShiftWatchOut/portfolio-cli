d9d628c81dca9a403889f42cb164dd3552ea6095
watchout <2080717464@qq.com>
2022-06-02 17:00:01 +0800

    ✨ feat: 新增多选，获取时间，拼接tag数组

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,92 @@
 use std::{env, fs, io};
 
+use chrono::prelude::*;
 use console::style;
-use dialoguer::Input;
+use dialoguer::{Input, MultiSelect};
 
-struct TextConfig<'a> {
+enum PromptType {
+    Input,
+    Outter,
+    Multiselect,
+}
+
+struct PromptConfig<'a> {
     name: &'a str,
-    type_name: &'a str,
+    type_name: PromptType,
     message: &'a str,
     initial: &'a str,
 }
 
 fn run() -> io::Result<()> {
+    let current_day = Local::now().naive_local().date().to_string();
+    println!("今天是 {}", current_day);
     for arg in env::args() {
         println!("arg: {}", style(arg).green());
     }
     let source = fs::read_to_string("./template.md").unwrap();
-    let first_config = TextConfig {
-        name: "title",
-        type_name: "text",
-        message: "博文标题",
-        initial: "New Post",
-    };
-    let input = Input::<String>::new()
-        .with_prompt(first_config.message)
-        .with_initial_text(first_config.initial)
-        .interact_text()?;
-    println!("prompt result: {}", input);
-    let target_file_name = "hello.md";
-    let new_content = source.as_str();
-    let new_content = new_content.replace(
-        ("{{ ".to_owned() + first_config.name + " }}").as_str(),
-        input.as_str(),
-    );
+    let configs = vec![
+        // PromptConfig {
+        //     name: "title",
+        //     type_name: PromptType::Input,
+        //     message: "博文标题",
+        //     initial: "New Post",
+        // },
+        // PromptConfig {
+        //     name: "filename",
+        //     type_name: PromptType::Input,
+        //     message: "MD 文件名(需要后缀)",
+        //     initial: "new-post.md",
+        // },
+        // PromptConfig {
+        //     name: "description",
+        //     type_name: PromptType::Input,
+        //     message: "简单说明",
+        //     initial: "description text",
+        // },
+        PromptConfig {
+            name: "date",
+            type_name: PromptType::Outter,
+            message: "来自外部的时间",
+            initial: &current_day,
+        },
+        PromptConfig {
+            name: "tag",
+            type_name: PromptType::Multiselect,
+            message: "标签分类",
+            initial: "",
+        },
+    ];
+    let mut new_content = source;
+    for config in configs {
+        let input: String;
+        match config.type_name {
+            PromptType::Input => {
+                input = Input::<String>::new()
+                    .with_prompt(config.message)
+                    .with_initial_text(config.initial)
+                    .interact_text()?;
+            }
+            PromptType::Multiselect => {
+                let existing_tags = vec!["Option 1", "Option 2"];
+                let chosen: Vec<usize> = MultiSelect::new().items(&existing_tags).interact()?;
+                println!("multi select result: {:?}", chosen);
+                input = chosen
+                    .iter()
+                    .map(|i| existing_tags[*i])
+                    .collect::<Vec<_>>()
+                    .join(", ");
+            }
+            PromptType::Outter => {
+                input = String::from(config.initial);
+            }
+        }
+        println!("prompt result: {}", input);
+        new_content = new_content.replace(
+            ("{{ ".to_owned() + config.name + " }}").as_str(),
+            input.as_str(),
+        );
+    }
+    let target_file_name = "md_target/hello.md";
     // println!("doc: \n{}", new_content);
     fs::write(target_file_name, new_content)?;
     Ok(())
